2f736b3f8d3a9d6f0abb418206ca9f71cb4e3c58
gdrosos <t8180024@aueb.gr>
2019-11-11 18:58:54 +0200

    Added toString and created second constructor for loading objects from csv file

--- src/main/java/RegisteredCustomer.java
+++ src/main/java/RegisteredCustomer.java
@@ -3,18 +3,29 @@ public class RegisteredCustomer extends Customer {
 	//instance variables
 	private int id;
 	private int points;
-	//staitc variables
-	private static int idcounter = 0;
+	// counts the number of Registered Customers created
+	private static int idcounter;
 	private static ArrayList<RegisteredCustomer> customers = new ArrayList<RegisteredCustomer>();
+	// new registered customer
 	public RegisteredCustomer(String name, String surname, String adress, int telephone, int points) {
+			super(name, surname, adress, telephone);
+			this.points = points;
+			this.id = ++idcounter;
+			//adding registered customer to arraylist
+			customers.add(this);
+			idcounter++;
+			//adding customer to csv file
+			FileHandler.writeToCSV(this);
+		}
+	//Constructor for loading customers read from .csv file
+	//The id is already associated the customer
+	public RegisteredCustomer(String name, String surname, String adress, int telephone, int id, int points) {
 		super(name, surname, adress, telephone);
+		this.id = id;
 		this.points = points;
-		this.id = idcounter;
-		//adding registered customer to arraylist
-		customers.add(this);
-		idcounter++;
-		//adding customer to csv file
-		FileHandler.writeToCSV(this);
 	}
-
+	@Override
+	public String toString() {
+		return super.toString()+" id=" + id + ", points=" + points + "]";
+	}
 }
