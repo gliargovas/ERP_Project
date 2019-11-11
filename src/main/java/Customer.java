53eb83c4e74f61189d222b9839d8b3bd827f1eef
gdrosos <t8180024@aueb.gr>
2019-11-11 17:54:56 +0200

    created Customer Class
    added instance variables and setters and getters

---
+++ src/main/java/Customer.java
@@ -0,0 +1,33 @@
+public class Customer {
+	//instance variables
+	private String name;
+	private String surname;
+	private String adress;
+	private int telephone;
+	//getters and setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public int getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+	
+}
