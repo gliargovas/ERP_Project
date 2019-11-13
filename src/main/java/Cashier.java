97a88eb9cdee71d3736c65b8e7861b9c1afe6fe2
Dimitris Tsaousis <dimitriostsaousis00@gmail.com>
2019-11-13 18:06:01 +0200

    First creation of Cashier, Storekeeper, DataAnalyst classes

---
+++ src/main/java/Cashier.java
@@ -0,0 +1,10 @@
+
+public class Cashier extends User {
+	public Cashier(int idUser, String name, String surname, String username, String password) {
+		super(idUser, name, surname, username, password);
+	}
+	@Override
+	public void getMenu() {
+		
+	}
+}
---
+++ src/main/java/Storekeeper.java
@@ -0,0 +1,10 @@
+
+public class Storekeeper extends User{
+	public Storekeeper(int idUser, String name, String surname, String username, String password) {
+		super(idUser, name, surname, username, password);
+	}
+	@Override
+	public void getMenu() {
+		
+	}
+}
