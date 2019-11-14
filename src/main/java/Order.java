d21a631a9e11dd3035b96e23cbadc643cb4afdb1
eirinip1905 <t8180106@aueb.gr>
2019-11-14 20:40:24 +0200

    Order
    Add Arraylist orders and add final typification to the variables

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,47 +1,44 @@
 import java.sql.Date;
+import java.util.ArrayList;
 
 public class Order {
-	private int OrderNo;
-	private Date OrderDate;
-	private double TotalCost;
-	private Customer customer;
-	private Cashier cashier;
+	private final int OrderNo;
+	private final int count;
+	private final Date OrderDate;
+	private final double TotalCost;
+	private final Customer customer;
+	private final Cashier cashier;
+	protected ArrayList<Order> orders=new ArrayList<Order>();
+	
 	public Order(int orderNo, Date orderDate, double totalCost, Customer customer, Cashier cashier) {
 		super();
-		OrderNo = orderNo;
 		OrderDate = orderDate;
 		TotalCost = totalCost;
 		this.customer = customer;
 		this.cashier = cashier;
+		count++;
+		OrderNo=count;
+		orders.add(this);
+		FileHandler.writeToCSV(this);
 	}
+	
 	public int getOrderNo() {
 		return OrderNo;
 	}
-	public void setOrderNo(int orderNo) {
-		OrderNo = orderNo;
-	}
+	
 	public Date getOrderDate() {
 		return OrderDate;
 	}
-	public void setOrderDate(Date orderDate) {
-		OrderDate = orderDate;
-	}
+	
 	public double getTotalCost() {
 		return TotalCost;
 	}
-	public void setTotalCost(double totalCost) {
-		TotalCost = totalCost;
-	}
+
 	public Customer getCustomer() {
 		return customer;
 	}
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
+
 	public Cashier getCashier() {
 		return cashier;
-	}
-	public void setCashier(Cashier cashier) {
-		this.cashier = cashier;
 	}
 }
