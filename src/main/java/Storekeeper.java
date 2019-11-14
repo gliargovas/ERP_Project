d1b685cf9fb048ff7540c414320386c553b58ffa
nemod <nemod@192.168.1.67>
2019-11-14 16:59:26 +0200

    create the getMenu method
    shows the menu and let the user to select

--- src/main/java/Storekeeper.java
+++ src/main/java/Storekeeper.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class Storekeeper extends User{
 	public Storekeeper(int idUser, String name, String surname, String username, String password) {
 		super(idUser, name, surname, username, password);
@@ -13,7 +13,46 @@ public class Storekeeper extends User{
 		System.out.println("	  5.Exit");
 	}
 	@Override
-	public void getMenu() { 
+	public void getMenu() { int ch;
+	 int yes=0;
+	 Scanner sc=new Scanner(System.in); 
+	  
+	try{ 
+	
+	  while(yes==0){ 
+		  	showMenu();
+	        System.out.print("Enter your choice:"); 
+	        ch=sc.nextInt(); 
+	        switch(ch){ 
+	             case 1: 
+	                    //Method that shows storage info
+	                    break; 
+	            case 2: 
+	                    //Search method 
+	                     break; 
+	           case 3: 
+
+	                    //Method that add products 
+	                    break; 
+	           case 4:                     
+	                    //Method that deletes products      
+	                     break; 
+	                     
+	           case 5: System.exit(0); 
+	  
+	          default: System.out.println("Invalid choice!"); 
+	  
+	} 
+	  
+	    
+	   System.out.print("Continue? Press 0 to continue:"); 
+	    yes=sc.nextInt(); 
+	  
+	 } 
+	  
+	 }catch(Exception e){
+		 System.exit(100);
+	 }
 		   
 		  
 		}
