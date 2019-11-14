9265fc347068cbb83eb19271425f46598557bab1
nemod <nemod@192.168.1.67>
2019-11-14 15:48:45 +0200

    create showMenu
    it's a void method that shows the menu

--- src/main/java/Storekeeper.java
+++ src/main/java/Storekeeper.java
@@ -3,8 +3,20 @@ public class Storekeeper extends User{
 	public Storekeeper(int idUser, String name, String surname, String username, String password) {
 		super(idUser, name, surname, username, password);
 	}
+	private void showMenu() {
+		System.out.println("Storekeeper's Menu");
+		System.out.println("press:");
+		System.out.println("      1.Storage info");
+		System.out.println("	  2.Find an item");
+		System.out.println("	  3.Add product");
+		System.out.println("	  4.Delete product");
+		System.out.println("	  5.Exit");
+	}
 	@Override
-	public void getMenu() {
+	public void getMenu() { 
+		   
+		  
+		}
 		
 	}
-}
+
