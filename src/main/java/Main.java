919d2770ad9612616ebd94768ba6f3a02cca3650
gliargovas <t8180061@aueb.gr>
2019-11-16 11:35:42 +0200

    Create Main class with all its methods
    Main class contains the main menu and all the appropriate methods for
    the functions of the main menu.

---
+++ src/main/java/Main.java
@@ -0,0 +1,144 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		loadAllListsFromCsv();
+		getMainMenu();
+	}
+
+	public static void printMainMenu() {
+		System.out.print("ERP SYSTEM\n\n"
+				+ "1) Login as a Cashier\n"
+				+ "2  Login as a Storekeeper"
+				+ "3) Login as a Data Analyst "
+				+ "4) Login as Administrator\n"
+				+ "5) Exit Application\n\n"
+				+ "\tAnswer: )");
+	}
+	
+	public static void getMainMenu() {
+		Scanner in = new Scanner(System.in);
+		int ans;
+		for(;;) {
+			printMainMenu();
+			try {
+				ans = in.nextInt();
+				in.nextLine();
+				switch (ans) {
+				case 1: 
+					cashierLogin();
+					break;
+				case 2:
+					storekeeperLogin();
+					break;
+				case 3:
+					dataAnalystLogin();
+					break;
+				case 4:
+					administratorLogin();
+					break;
+				case 5:
+					saveAllListsToCsv();
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Prease enter a valid option. Try again...");
+				}
+			}
+			catch (InputMismatchException e) {
+				in.nextLine();
+				System.out.println("Your option must be an integer number. Try again...");
+			}
+			catch (Exception e) {
+				System.out.println("An error occured. Try again...");
+			}
+		}
+	}
+	
+	public static void cashierLogin() {
+		Scanner in = new Scanner(System.in);
+		String username, password;
+		try {
+			System.out.print("Enter your username.\n"
+					+ "To exit press \"enter\": ");
+			username = in.nextLine();
+			if (username.equals("")) {
+				System.out.println("Returning to main menu...");
+				return;
+			}
+			System.out.print("Enter your password: ");
+			password = in.nextLine();
+			Cashier.login(username, password);
+		} catch (Exception e) {
+			System.out.println("Invalid credentials given. Try again...");
+		}
+	}
+	
+	public static void storekeeperLogin() {
+		Scanner in = new Scanner(System.in);
+		String username, password;
+		try {
+			System.out.print("Enter your username.\n"
+					+ "To exit press \"enter\": ");
+			username = in.nextLine();
+			if (username.equals("")) {
+				System.out.println("Returning to main menu...");
+				return;
+			}
+			System.out.print("Enter your password: ");
+			password = in.nextLine();
+			Storekeeper.login(username, password);
+		} catch (Exception e) {
+			System.out.println("Invalid credentials given. Try again...");
+		}
+	}
+	
+	public static void dataAnalystLogin() {
+		Scanner in = new Scanner(System.in);
+		String username, password;
+		try {
+			System.out.print("Enter your username.\n"
+					+ "To exit press \"enter\": ");
+			username = in.nextLine();
+			if (username.equals("")) {
+				System.out.println("Returning to main menu...");
+				return;
+			}
+			System.out.print("Enter your password: ");
+			password = in.nextLine();
+			DataAnalyst.login(username, password);
+		} catch (Exception e) {
+			System.out.println("Invalid credentials given. Try again...");
+		}
+	}
+	
+	public static void administratorLogin() {
+		Scanner in = new Scanner(System.in);
+		String username, password;
+		try {
+			System.out.print("Enter your username.\n"
+					+ "To exit press \"enter\": ");
+			username = in.nextLine();
+			if (username.equals("")) {
+				System.out.println("Returning to main menu...");
+				return;
+			}
+			System.out.print("Enter your password: ");
+			password = in.nextLine();
+			Administrator.login(username, password);
+		} catch (Exception e) {
+			System.out.println("Invalid credentials given. Try again...");
+		}
+	}
+	
+	private static void saveAllListsToCsv() {
+		//TODO Complete the method when all the classes have been connected
+	}
+	
+	private static void loadAllListsFromCsv() {
+		// TODO Complete the method when all the classes have been connected
+	}
+
+}
