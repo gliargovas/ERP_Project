e74d686a75830596cec2cf4b8170ab6462860ad9
gliargovas <t8180061@aueb.gr>
2019-11-16 12:00:06 +0200

    Add cashier changes
    Forgot to add cashier class to the previous commit

--- src/main/java/Cashier.java
+++ src/main/java/Cashier.java
@@ -1,8 +1,28 @@
 
 public class Cashier extends User {
+	
+	public Cashier(String name, String surname, String username, String password) {
+		super(name, surname, username, password);
+	}
+	
 	public Cashier(int idUser, String name, String surname, String username, String password) {
 		super(idUser, name, surname, username, password);
 	}
+	
+	public static void login(String username, String password) throws Exception {
+		Cashier cashier;
+		for (User user : User.getUsers()) {
+			if (user instanceof Cashier) {
+				if (username.equals(user.getName()) && password.equals(user.getPassword())) {
+					cashier = (Cashier)user;
+					cashier.getMenu();
+					return;
+				}
+			}
+		}
+		throw new Exception("Invalid Credentials");
+	}
+	
 	@Override
 	public void getMenu() {
 		
