07583544fa6479e8a57071456846adf88274ec1e
ChristinaKardami <christina.kardami@gmail.com>
2019-11-20 01:15:59 +0200

    Add methods
    Add constructor, alltheSuppliers method, delete Supplier, search by id
    and create Supplier from list.

--- src/main/java/Supplier.java
+++ src/main/java/Supplier.java
@@ -6,7 +6,16 @@ public class Supplier {
 	private String Address;
 	private static int idCounter;
 	private static ArrayList<Supplier> Suppliers = new ArrayList<Supplier>();
-	public Supplier(String name, int id, int tel, String address) {
+	
+	public Supplier(String name, String address, int tel, int id) {
+		super();
+		Name = name;
+		Id = id;
+		Tel = tel;
+		Address = address;
+		Suppliers.add(this);
+	}
+	public Supplier(String name,int tel, String address) {
 		Name = name;
 		Id = ++idCounter;
 		Tel = tel;
@@ -41,11 +50,46 @@ public class Supplier {
 	public static ArrayList<Supplier> getSuppliers() {
 		return Suppliers;
 	}
-	private 
+	public static void alltheSuppliers() {
+		for (Supplier k : Suppliers) {
+			k.toString();
+		}
+	}
+	public static boolean deleteSupplier(int id) {
+		for(Supplier i : Suppliers) {
+			if(i.getId() == id) {
+				Suppliers.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+	public static Supplier searchbyId(int id) {
+		for (Supplier i : Suppliers) {
+			if (i.getId() == id) {
+				return i;
+			}
+		}
+		System.out.println("There is no such element");
+		return null;
+	}
+	public static void createSuppliersFromList(ArrayList<ArrayList<String>> Suppliers) {
+		int Id, Tel;
+		String Name, Address;
+		for (ArrayList<String> supplier: Suppliers) {
+			Id = Integer.parseInt(supplier.get(0));
+			Name = supplier.get(1);
+			Address = supplier.get(2);
+			Tel = Integer.parseInt(supplier.get(3));
+			new Supplier(Name, Address,Tel, Id);
+		}
+	}
 	@Override
 	public String toString() {
 		return "Supplier [Name=" + Name + ", Id=" + Id + ", Tel=" + Tel + ", Address=" + Address + "]";
 	}
+	 
+	
 	
 
 }
