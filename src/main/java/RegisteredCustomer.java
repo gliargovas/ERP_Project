e8b8ef39ae81ed362e826ab8824cc6f38b7584d8
gdrosos <t8180024@aueb.gr>
2019-11-25 02:50:58 +0200

    added delete customer menu

--- src/main/java/RegisteredCustomer.java
+++ src/main/java/RegisteredCustomer.java
@@ -85,177 +85,209 @@ public class RegisteredCustomer extends Customer {
 		}
 	}
 	// Deletes a customer from list
-		public static void deleteRegisteredCustomer(int id) throws NoSuchElementException {
-			RegisteredCustomer customer = searchById(id);
-			if (customer == null) {
-				throw new NoSuchElementException("Customer with such id does not exist");
-			}
-				customers.remove(customers.indexOf(customer));
+	public static void deleteRegisteredCustomer(int id) throws NoSuchElementException {
+		RegisteredCustomer customer = searchById(id);
+		if (customer == null) {
+			throw new NoSuchElementException("Customer with such id does not exist");
 		}
-		//checks if a Registered Customer already exists and if it does, changes his points
-		public static void changePoints(int id, int points) throws NoSuchElementException, NumberFormatException{
-			if (points <= 0) {
-				throw new NumberFormatException("Points must be more or equal to 0");
-			}
-			RegisteredCustomer customer = searchById(id);
-			if (customer == null) {
-				throw new NoSuchElementException("Customer with such id does not exist");
-			} else {
-			customer.setPoints(points);
-			}
+			customers.remove(customers.indexOf(customer));
+	}
+	//checks if a Registered Customer already exists and if it does, changes his points
+	public static void changePoints(int id, int points) throws NoSuchElementException, NumberFormatException{
+		if (points <= 0) {
+			throw new NumberFormatException("Points must be more or equal to 0");
 		}
+		RegisteredCustomer customer = searchById(id);
+		if (customer == null) {
+			throw new NoSuchElementException("Customer with such id does not exist");
+		} else {
+		customer.setPoints(points);
+		}
+	}
 		//checks if a Registered Customer already exists and if it does, changes his name
-		public static void changeName(int id,String name) throws NoSuchElementException{
-			RegisteredCustomer customer = searchById(id);
-			if (customer == null) {
-				throw new NoSuchElementException("Customer with such id does not exist");
-			} else {
-				customer.setName(name);
+	public static void changeName(int id,String name) throws NoSuchElementException{
+		RegisteredCustomer customer = searchById(id);
+		if (customer == null) {
+			throw new NoSuchElementException("Customer with such id does not exist");
+		} else {
+			customer.setName(name);
+		}
+	}
+	//checks if a Registered Customer already exists and if it does, changes his address
+	public static void changeAddress(int id,String address) throws NoSuchElementException{
+		RegisteredCustomer customer = searchById(id);
+		if (customer == null) {
+			throw new NoSuchElementException("Customer with such id does not exist");
+		} else {
+			customer.setAddress(address);
+		}
+	}
+	//checks if a Registered Customer already exists and if it does, changes his telephone
+	public static void changeTelephone(int id,int telephone) throws NoSuchElementException{
+		RegisteredCustomer customer = searchById(id);
+		if (customer == null) {
+			throw new NoSuchElementException("Customer with such id does not exist");
+		} else {
+			customer.setTelephone(telephone);
+		}
+	}
+	// contains the registered customer creation menu TODO Customer menu at superclass
+	public static void registerNewCustomerMenu() {
+		String name, address;
+		int telephone,points;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the customer's name: ");
+		name = in.nextLine();
+		System.out.print("Enter the customer's address: ");
+		address = (in.nextLine().toLowerCase());
+		System.out.print("Enter customer's telephone: ");
+		telephone = in.nextInt();
+		for (;;) {	
+			System.out.print("Enter customer's points: ");
+			try {
+				points = in.nextInt();
+				if (points <= 0) {
+					System.out.println("Points must be larger than 0. Try again...");
+					continue;
+				}
+				break;
+			} catch(InputMismatchException e) {
+				System.err.println("Invalid input given. Points must be a number");
+				in.nextLine();
 			}
 		}
-			//checks if a Registered Customer already exists and if it does, changes his address
-			public static void changeAddress(int id,String address) throws NoSuchElementException{
-				RegisteredCustomer customer = searchById(id);
-				if (customer == null) {
-					throw new NoSuchElementException("Customer with such id does not exist");
-				} else {
-					customer.setAddress(address);
+		new RegisteredCustomer(name,address,telephone,points);
+		System.out.printf("Customer %s registered successfully!\n", name);
+	}
+	// contains the customer points change menu
+	public static void changeCustomerPointsMenu() {
+		Scanner in = new Scanner(System.in);
+		int id, points;
+		String input;
+		for(;;) {
+			id = 0;
+			try {
+				System.out.print("Enter the id of the registered customer you want to change\nTo cancel, press \"enter\": ");
+				input = in.nextLine();
+				if (input.equals("")) {
+					System.out.println("Process cancelled. Returning to previous menu...");
+					return;
 				}
-		}		
-		// contains the registered customer creation menu TODO Customer menu at superclass
-		public static void registerNewCustomerMenu() {
-			String name, address;
-			int telephone,points;
-			Scanner in = new Scanner(System.in);
-			System.out.print("Enter the customer's name: ");
-			name = in.nextLine();
-			System.out.print("Enter the customer's address: ");
-			address = (in.nextLine().toLowerCase());
-			System.out.print("Enter customer's telephone: ");
-			telephone = in.nextInt();
-			for (;;) {	
-				System.out.print("Enter customer's points: ");
-				try {
-					points = in.nextInt();
-					if (points <= 0) {
-						System.out.println("Points must be larger than 0. Try again...");
-						continue;
-					}
-					break;
-				} catch(InputMismatchException e) {
-					System.err.println("Invalid input given. Points must be a number");
-					in.nextLine();
+				id = Integer.parseInt(input);
+				System.out.print("Enter new points: ");
+				points = in.nextInt();
+				changePoints(id,points);
+				break;
+			} catch (NumberFormatException e){
+				System.err.println("Invalid input given. Id must be an integer, larger than 0");
+			} catch (InputMismatchException e) {
+				System.err.println("Invalid input given. Points must be a number");
+			} catch (NoSuchElementException e) {
+				System.err.println(e.getMessage());
 				}
 			}
-			new RegisteredCustomer(name,address,telephone,points);
-			System.out.printf("Customer %s registered successfully!\n", name);
-		}
-		// contains the customer points change menu
-		public static void changeCustomerPointsMenu() {
-			Scanner in = new Scanner(System.in);
-			int id, points;
-			String input;
-			for(;;) {
-				id = 0;
-				try {
-					System.out.print("Enter the id of the registered customer you want to change\nTo cancel, press \"enter\": ");
-					input = in.nextLine();
-					if (input.equals("")) {
-						System.out.println("Process cancelled. Returning to previous menu...");
-						return;
-					}
-					id = Integer.parseInt(input);
-					System.out.print("Enter new points: ");
-					points = in.nextInt();
-					changePoints(id,points);
-					break;
-				} catch (NumberFormatException e){
-					System.err.println("Invalid input given. Id must be an integer, larger than 0");
-				} catch (InputMismatchException e) {
-					System.err.println("Invalid input given. Points must be a number");
-				} catch (NoSuchElementException e) {
-					System.err.println(e.getMessage());
-					}
+	}
+	// contains the customer name change menu
+	public static void changeCustomerNameMenu() {
+		Scanner in = new Scanner(System.in);
+		int id;
+		String input, name;
+		for(;;) {
+			id = 0;
+			try {
+				System.out.print("Enter the id of the registered customer you want to change\nTo cancel, press \"enter\": ");
+				input = in.nextLine();
+				if (input.equals("")) {
+					System.out.println("Process cancelled. Returning to previous menu...");
+					return;
 				}
-		}
-		// contains the customer name change menu
-		public static void changeCustomerNameMenu() {
-			Scanner in = new Scanner(System.in);
-			int id;
-			String input, name;
-			for(;;) {
-				id = 0;
-				try {
-					System.out.print("Enter the id of the registered customer you want to change\nTo cancel, press \"enter\": ");
-					input = in.nextLine();
-					if (input.equals("")) {
-						System.out.println("Process cancelled. Returning to previous menu...");
-						return;
-					}
-					id = Integer.parseInt(input);
-					System.out.print("Enter the new name: ");
-					name = in.nextLine();
-					changeName(id,name);
-					break;
-				} catch (NumberFormatException e){
-					System.err.println("Invalid input given. Id must be an integer, larger than 0");
-				} catch (NoSuchElementException e) {
-					System.err.println(e.getMessage());
-					}
+				id = Integer.parseInt(input);
+				System.out.print("Enter the new name: ");
+				name = in.nextLine();
+				changeName(id,name);
+				break;
+			} catch (NumberFormatException e){
+				System.err.println("Invalid input given. Id must be an integer, larger than 0");
+			} catch (NoSuchElementException e) {
+				System.err.println(e.getMessage());
 				}
-		}
-		// contains the customer address change menu
-		public static void changeCustomerAddressMenu() {
-			Scanner in = new Scanner(System.in);
-			int id;
-			String input, address;
-			for(;;) {
-				id = 0;
-				try {
-					System.out.print("Enter the id of the registered customer you want to change\nTo cancel, press \"enter\": ");
-					input = in.nextLine();
-					if (input.equals("")) {
-						System.out.println("Process cancelled. Returning to previous menu...");
-						return;
-					}
-					id = Integer.parseInt(input);
-					System.out.print("Enter the new adress: ");
-					address = in.nextLine();
-					changeAddress(id,address);
-					break;
-				} catch (NumberFormatException e){
-					System.err.println("Invalid input given. Id must be an integer, larger than 0");
-				} catch (NoSuchElementException e) {
-					System.err.println(e.getMessage());
-					}
+			}
+	}
+	// contains the customer address change menu
+	public static void changeCustomerAddressMenu() {
+		Scanner in = new Scanner(System.in);
+		int id;
+		String input, address;
+		for(;;) {
+			id = 0;
+			try {
+				System.out.print("Enter the id of the registered customer you want to change\nTo cancel, press \"enter\": ");
+				input = in.nextLine();
+				if (input.equals("")) {
+					System.out.println("Process cancelled. Returning to previous menu...");
+					return;
 				}
-		}
-		//contains customer telephone change menu
-		public static void changeCustomerTelephoneMenu() {
-			Scanner in = new Scanner(System.in);
-			int id, telephone;
-			String input;
-			for(;;) {
-				id = 0;
-				try {
-					System.out.print("Enter the id of the registered customer you want to change\nTo cancel, press \"enter\": ");
-					input = in.nextLine();
-					if (input.equals("")) {
-						System.out.println("Process cancelled. Returning to previous menu...");
-						return;
-					}
-					id = Integer.parseInt(input);
-					System.out.print("Enter new telephone: ");
-					telephone = in.nextInt();
-					searchById(id).setTelephone(telephone);
-					break;
-				} catch (NumberFormatException e){
-					System.err.println("Invalid input given. Id must be an integer, larger than 0");
-				} catch (InputMismatchException e) {
-					System.err.println("Invalid input given. Telephone must be a number");
-				} catch (NoSuchElementException e) {
-					System.err.println(e.getMessage());
-					}
+				id = Integer.parseInt(input);
+				System.out.print("Enter the new adress: ");
+				address = in.nextLine();
+				changeAddress(id,address);
+				break;
+			} catch (NumberFormatException e){
+				System.err.println("Invalid input given. Id must be an integer, larger than 0");
+			} catch (NoSuchElementException e) {
+				System.err.println(e.getMessage());
 				}
-		}	
+			}
+	}
+	//contains customer telephone change menu
+	public static void changeCustomerTelephoneMenu() {
+		Scanner in = new Scanner(System.in);
+		int id, telephone;
+		String input;
+		for(;;) {
+			id = 0;
+			try {
+				System.out.print("Enter the id of the registered customer you want to change\nTo cancel, press \"enter\": ");
+				input = in.nextLine();
+				if (input.equals("")) {
+					System.out.println("Process cancelled. Returning to previous menu...");
+					return;
+				}
+				id = Integer.parseInt(input);
+				System.out.print("Enter new telephone: ");
+				telephone = in.nextInt();
+				changeTelephone(id,telephone);
+				break;
+			} catch (NumberFormatException e){
+				System.err.println("Invalid input given. Id must be an integer, larger than 0");
+			} catch (InputMismatchException e) {
+				System.err.println("Invalid input given. Telephone must be a number");
+			} catch (NoSuchElementException e) {
+				System.err.println(e.getMessage());
+				}
+			}
+	}
+	public static void deleteCustomerMenu() {
+		Scanner in = new Scanner(System.in);
+		int id;
+		String input;
+		for(;;) {
+			id = 0;
+			try {
+				System.out.print("Enter the id of the registered customer you want to delete\nTo cancel, press \"enter\": ");
+				input = in.nextLine();
+				if (input.equals(" ")) {
+					System.out.println("Process cancelled. Returning to previous menu...");
+					return;
+				}
+				id = Integer.parseInt(input);
+				deleteRegisteredCustomer(id);
+				break;
+			} catch (NoSuchElementException e) {
+				System.err.println(e.getMessage());
+			} catch (Exception e) {
+				System.err.println("An error occured. Returning to previous menu");
+			}
+		}
+	}
 }
