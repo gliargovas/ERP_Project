8070881ea1f263e48132506bfc3f67740801eea0
gliargovas <t8180061@aueb.gr>
2019-12-01 12:30:33 +0200

    Make corrections to order class

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
@@ -8,38 +9,31 @@ public class Order {
 	private final double totalCost;
 	private final Customer customer;
 	private final Cashier cashier;
-	private final String address;
 	private ArrayList<int[]> basket=new ArrayList<int[]>();
 	private static int count = 1;
 	private  static ArrayList<Order> orders=new ArrayList<Order>();
  	
 	//Create a new order
-	public Order(Date orderDate, Customer customer, Cashier cashier, String address, ArrayList<int[]> basket) {
+	public Order(Date orderDate, Customer customer, Cashier cashier, ArrayList<int[]> basket) {
 		this.orderNo = count++;
 		this.orderDate = orderDate;
 		this.totalCost = this.calculateBasketCost(basket);
 		this.customer = customer;
 		this.cashier = cashier;
-		this.address = address;
 		this.basket = basket;
 		orders.add(this);
 	}
 	
 	/*Constructor for already created products read from .csv file
 	There is non need to calculate the total cost again as it has been created*/
-	public Order(int orderNo, Date orderDate, double totalCost, Customer customer, Cashier cashier, String address, ArrayList<int[]> basket) {
+	public Order(int orderNo, Date orderDate, double totalCost, Customer customer, Cashier cashier, ArrayList<int[]> basket) {
 		this.orderNo = orderNo;
 		this.orderDate = orderDate;
 		this.totalCost = totalCost;
 		this.customer = customer;
 		this.cashier = cashier;
-		this.address = address;
 		this.basket = basket;
 		orders.add(this);
-	}
-	
-	public String getAddress() {
-		return address;
 	}
 
 	//Getters and Setters
@@ -71,11 +65,10 @@ public class Order {
 		return orders;
 	}
 	
-	//Basket creation 
-	public void addProductToBasket(int Id, int quantity) {
-		Product prod = Storage.searchById(id);
-		int[] prod= {Id, quantity};
-		basket.add(prod);
+	//Add product to basket 
+	public void addProductToBasket(int id, int quantity) {
+		int[] products= {id, quantity};
+		basket.add(products);
 	}
 	
 	//Calculate a product's cost 
@@ -89,10 +82,12 @@ public class Order {
 	//Calculates the total cost of an order's basket
 	public static double calculateBasketCost (ArrayList<int[]> basket) {
 		double totalCost = 0;
-		Product prod = Storage.searchById(id);
+		double productCost;
+		Product prod;
 		for (int[] product : basket) {
 			prod = Storage.searchById(product[0]);
-			totalCost += prod.getSalePrice();
+			productCost = prod.getSalePrice() * product[1];
+			totalCost += productCost;
 		}
 		return totalCost;
 	}
@@ -109,26 +104,26 @@ public class Order {
 	}
 		
 	//Prints a preview of the Order
-	public void previewOrder (Cashier cashier, Customer customer, String address ArrayList<int[]> basket) {
+	public void previewOrder (Cashier cashier, Customer customer, ArrayList<int[]> basket) {
 		int cashierId = cashier.getIdUser();
 		double totalCost = 0;
 		System.out.println("Preview of Order");
 		System.out.println();
 		System.out.println("Cashier's Code: " + cashierId);
 		System.out.println("Customer's data");
-		if (Customer instanceof RegisteredCustomer) {
-			String customername = get.CompanyName();
-			System.out.println("Name: " + customername);
+		if (customer instanceof RegisteredCustomer) {
+			String customerName = customer.getCompanyName();
+			System.out.println("Name: " + customerName);
 		} else {
 			System.out.println("Guest");
 		}
-		System.out.println("Address: " + address);
+		System.out.println("Address: " + customer.getAddress());
 		System.out.println("Products: ");
 		for (int prod [] : basket) {
 			printProduct(prod [0], prod [1]);
 		}
-		totalcost = calculateBasketCost(basket);
-		printTotalCost (totalcost);
+		double totalcost = calculateBasketCost(basket);
+		System.out.println("Order's total cost: " + totalCost);	
 	}
 	
 	//Prints final Order
@@ -139,25 +134,20 @@ public class Order {
 		System.out.println();
 		System.out.println();
 		System.out.println("OrderNo: " + orderNo);
-		System.out.println("Cashier's Code: " + cashId);
+		System.out.println("Cashier's Code: " + cashierId);
 		System.out.println("Customer's data");
-		if (Customer instanceof RegisteredCustomer) {
-			String customername = get.CompanyName();
-			System.out.println("Name: " + customername);
+		if (customer instanceof RegisteredCustomer) {
+			String customerName = customer.getCompanyName();
+			System.out.println("Name: " + customerName);
 		} else {
 			System.out.println("Guest");
 		}
-		System.out.println("Address: " + address);
+		System.out.println("Address: " + customer.getAddress());
 		System.out.println("Products: ");
 		for (int prod [] : basket) {
 			printProduct(prod [0], prod [1]);
 		}
 		totalcost = calculateBasketCost(basket);
-		printTotalCost (totalcost);
-	}
-	
-	//Prints total cost 
-	public void printTotalCost (double totalcost) {
 		System.out.println("Order's total cost: " + totalCost);	
 	}
 	
@@ -168,26 +158,31 @@ public class Order {
 		Date orderDate;
 		Customer customer;
 		Cashier cashier;
+		int[] temp = null;
 		SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MMM-yyyy");
 		for (ArrayList<String> order: orders) {
 			orderNo = Integer.parseInt(order.get(0));
-			orderDate = dateFormatter.parse(order.get(1));
+			try {
+				orderDate = dateFormatter.parse(order.get(1));
+			} catch (ParseException e) {
+				e.printStackTrace();
+				continue;
+			}
 			totalCost = Double.parseDouble(order.get(2));
 			customerId = Integer.parseInt(order.get(3));
 			basket = new ArrayList<int[]>();
-			int[] temp;
 			if (customerId == 0)
 				customer = null;
 			else
-				customer = RegisterdCustomer.searchById();
+				customer = RegisteredCustomer.searchById(customerId);
 			cashierId = Integer.parseInt(order.get(4));
-			cashier = User.searchUserById(cashierId);
+			cashier = (Cashier) User.searchUserById(cashierId);
 			for (int i = 5 ; i < orders.size(); i += 2) {
 				temp[0] = Integer.parseInt(order.get(i));
 				temp[1] = Integer.parseInt(order.get(i+1));
 				basket.add(temp);
 			}
-			new Order(orderNo, orderDate, customer, cashier, totalCost, basket);
+			new Order(orderNo, orderDate, totalCost, customer, cashier, basket);
 		}
 		
 	}
