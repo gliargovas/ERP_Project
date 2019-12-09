a471531296c3abfc988942c4dbc6ce9d44874c2f
ChristinaKardami <christina.kardami@gmail.com>
2019-12-09 12:11:40 +0200

    add methods

--- src/main/java/Supplier.java
+++ src/main/java/Supplier.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 public class Supplier {
 	private String Name;
 	private int Id;
@@ -83,6 +84,88 @@ public class Supplier {
 			Tel = Integer.parseInt(supplier.get(3));
 			new Supplier(Name, Address,Tel, Id);
 		}
+	}
+	public static void searchAndPrintRegisteredSupplierByName(String name) {
+		boolean found = false;
+		for (Supplier s : Suppliers) {
+			if (s.getName().toLowerCase().contains(name.toLowerCase())) {
+				found = true;
+				System.out.println(s);
+			}
+			if (found == false) {
+				System.out.println("No suppliers with such name");
+			}
+		}
+	}
+	public static void searchAndPrintSupplierrByAddress(String address) {
+		boolean found = false;
+		for (Supplier s : Suppliers) {
+			if (s.getAddress().toLowerCase().contains(address.toLowerCase())) {
+				found = true;
+				System.out.println(s);
+			}
+		}
+		if (found == false) {
+			System.out.println("No suppliers with such address");
+		}
+	}
+	public static void searchAndPrintSupplierrById(int id) {
+		for (Supplier s : Suppliers) {
+			if (s.getId() == id) {
+				System.out.println(s);
+				return;
+			}
+		}
+			System.out.println("No suppliers with such Id");
+	}
+	public static void searchAndPrintSupplierByTelephone(int tel) {
+		boolean found = false;
+		for (Supplier s : Suppliers) {
+			if (s.getTel() == tel) {
+				found = true;
+				System.out.println(s);
+			}
+		}
+		if (found == false) {
+			System.out.println("No suppliers with such telephone");
+		}
+	}
+	public static void changeName(int id,String name) throws NoSuchElementException{
+		Supplier supplier = searchbyId(id);
+		if (supplier == null) {
+			throw new NoSuchElementException("Supplier with such id does not exist");
+		} else {
+			supplier.setName(name);
+		}
+	}
+	public static void changeAddress(int id,String address) throws NoSuchElementException{
+		Supplier supplier = searchbyId(id);
+		if (supplier == null) {
+			throw new NoSuchElementException("Customer with such id does not exist");
+		} else {
+			supplier.setAddress(address);
+		}
+	}
+	public static void changeTelephone(int id,int tel) throws NoSuchElementException{
+		Supplier supplier = searchbyId(id);
+		if (supplier == null) {
+			throw new NoSuchElementException("Suppliers with such id does not exist");
+		} else {
+			supplier.setTel(tel);
+		}
+	}
+	public static void registerSupplierMenu() {
+		String name, address;
+		int tel;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the supplier's name: ");
+		name = in.nextLine();
+		System.out.print("Enter the supplier's address: ");
+		address = (in.nextLine().toLowerCase());
+		System.out.print("Enter supplier's telephone: ");
+		tel = in.nextInt();
+		new Supplier(name,tel,address);
+		System.out.printf("Supplier %s registered successfully!\n", name);
 	}
 	@Override
 	public String toString() {
