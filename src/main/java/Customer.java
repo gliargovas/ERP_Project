1a96f2e54187a04edf86ebf823e75f3ddda848aa
gdrosos <t8180024@aueb.gr>
2019-12-21 15:56:36 +0200

    added javadoc comments to Customer class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,7 +1,22 @@
+/**
+ * The class represents a non-registered corporate customer of the ERP system
+ * The class is responsible for creating a non-registered customer object and is used
+ * when a customer does not want to be registered and only his address and telephone
+ * are needed in order to complete the order and the shipping.
+ * @version     1.0
+ * @author      George Drosos
+ */
+
 public class Customer {
 	private String companyName;
 	private String address;
 	private int telephone;
+	/**
+	   * Constructor for creating a new non-registered customer.
+	   * @param name
+	   * @param address
+	   * @param telephone
+	   */
 	public Customer(String name, String address, int telephone) {
 		this.companyName = name;
 		this.address = address;
@@ -25,7 +40,9 @@ public class Customer {
 	public void setTelephone(int telephone) {
 		this.telephone = telephone;
 	}
-	//toString Method
+	/**
+	 * toString Method
+	 */
 	@Override
 	public String toString() {
 		return String.format("Name: %s | Address: %s | Telephone: %d ", this.getCompanyName(), this.getAddress(), this.getTelephone());
